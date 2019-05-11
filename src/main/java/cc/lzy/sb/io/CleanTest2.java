1f306d50f6f2681e51e938079323c29a527d031e
chengyu <chengyu@mogujie.com>
2019-05-11 16:52:08 +0800

    add

---
+++ src/main/java/cc/lzy/sb/io/CleanTest2.java
@@ -0,0 +1,11 @@
+package cc.lzy.sb.io;
+
+/**
+ * @author chengyu
+ * @version 19/5/11
+ */
+public class CleanTest2 {
+    public static void main(String[] args) {
+        System.out.println("This is Clean Test 2");
+    }
+}
